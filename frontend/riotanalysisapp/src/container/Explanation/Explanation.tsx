9023d4fbf6d23a8f91a41eee2ba7153905693e71
gkgkgkgkgk73 <gkgkgkgkgk73@gmail.com>
2023-01-03 19:26:50 +0900

    front end

--- frontend/riotanalysisapp/src/container/Explanation/Explanation.tsx
+++ frontend/riotanalysisapp/src/container/Explanation/Explanation.tsx
@@ -3,14 +3,20 @@ import { useDispatch } from 'react-redux';
 import './Explanation.css';
 import { AppDispatch } from '../../store';
 import Header from '../../component/Header/Header';
+import ItemInfo from '../../component/ItemInfo/ItemInfo';
+import DecInfo from '../../component/DecInfo/DecInfo';
+import AugmentInfo from '../../component/AugmentInfo/AugmentInfo';
 
 function Explanation(){
 
     const dispatch = useDispatch<AppDispatch>();
-    
+    const dd = [""]
     return(
         <div className='page'>
             <Header />
+            <ItemInfo itemName="itemName" itemImg="itemImg" itemDetail="itemDetail" itemCombination={dd} />
+            <DecInfo decName={dd} decImg={dd} decDetailUnit={dd} decDetailUnitImg={dd} decInfo="decInfo"/>
+            <AugmentInfo augmentName="augmentName" augmentImg="augmentImg" augmentInfo="augmentInfo"/>
         </div>
     );
 }
