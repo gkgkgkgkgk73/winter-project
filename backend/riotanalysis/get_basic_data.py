876081cb7c3074f35fc7c1041b3e0ff7ed29bf94
gkgkgkgkgk73 <gkgkgkgkgk73@gmail.com>
2023-01-04 19:30:06 +0900

    기본 자료 정리

--- backend/riotanalysis/get_basic_data.py
+++ backend/riotanalysis/get_basic_data.py
@@ -6,26 +6,94 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE","riotanalysis.settings")
 import django
 django.setup()
 
-from riotanalysisapp.models import MatchData
+from riotanalysisapp.models import BaseItem, UpperItem, Champion, Trait, Augment
 
 url = 'https://raw.communitydragon.org/12.23/cdragon/tft/ko_kr.json'
-
+icon_pre_url = 'https://raw.communitydragon.org/12.23/game/'
 resp = requests.get(url=url)
 data = resp.json()
 data_items = []
 
 def get_basic_items_info():
     for d in data['items']:
-        if 'TFT8' in d['apiName'] or 'TFT_' in d['apiName']:
-            data_items.append(d)
-    print(data_items)
+        if '_Item_' in d['apiName']:
+            if not d['from']:
+                BaseItem.objects.create(
+                    item_id = d['id'],
+                    item_name = d['name'],
+                    item_img = icon_pre_url+d['icon'].lower(),
+                    item_info = d['desc'],                   
+                    item_apiName = d['apiName'],
+                    item_effect = d['effects']
+                )
+    for d in data['items']:
+        if '_Item_' in d['apiName']:
+            if d['from']:
+                new_data = UpperItem.objects.create(
+                    item_id = d['id'],
+                    item_name = d['name'],
+                    item_img = icon_pre_url+d['icon'].lower(),
+                    item_info = d['desc'],                   
+                    item_apiName = d['apiName'],
+                    item_effect = d['effects'],
+                )
+                base_1 = BaseItem.objects.filter(item_id = d['from'][0])
+                base_2 = BaseItem.objects.filter(item_id = d['from'][1])
+                if base_1:
+                    new_data.base_items.add(base_1[0])
+                if base_2:
+                    new_data.base_items.add(base_2[0])
+                new_data.save()
+ #   for d in data_items:
+        
+    
     #여기 item 정보 저장하면 됨.
 
 def get_basic_champions_info():
     for d in data['setData']:
         for c in d['champions']:
-            if 'TFT8' in c['apiName'] or 'TFT_' in c['apiName']:
-                data_items.append(c)
-    print(data_items)
+            if c['traits']:
+                champ = Champion.objects.create(
+                    champion_apiName = c['apiName'],
+                    champion_variables = {'variables' : c['ability']['variables']},
+                    champion_name = c['name'],
+                    champion_img = icon_pre_url+c['icon'].lower(),
+                    champion_info = c['ability']['desc'],
+                    champion_stats = c['stats'],
+                    champion_cost = c['cost']
+                )
+                for t in c['traits']:
+                    temp = Trait.objects.filter(trait_name = t)
+                    if temp:
+                        champ.traits.add(temp[0])
+                champ.save()
     
+def get_basic_traits_info():
+    trait_list = data['setData']
+    for d in trait_list:
+        for t in d['traits']:
+            if 'Set8_' in t['apiName']:
+                Trait.objects.create(
+                        trait_name = t['name'],
+                        trait_apiName = t['apiName'],
+                        trait_img = icon_pre_url+t['icon'].lower(),
+                        trait_effect = {'effect': t['effects']},
+                        trait_info = t['desc']
+                    )
+
+def get_basic_augments_info():
+    for d in data['items']:
+        if '_Augment_' in d['apiName']:
+            Augment.objects.create(
+                augment_id = d['id'],
+                augment_name = d['name'],
+                augment_img = icon_pre_url+d['icon'].lower(),
+                augment_info = d['desc'],                   
+                augment_apiName = d['apiName'],
+                augment_effect = d['effects']
+            )
+            
+get_basic_items_info()
+get_basic_traits_info()
 get_basic_champions_info()
+get_basic_augments_info()
--- backend/riotanalysis/riotAnalysisApp/models.py
+++ backend/riotanalysis/riotAnalysisApp/models.py
@@ -2,33 +2,50 @@ from django.db import models
 
 #특성 기본 정보
 class Trait(models.Model):
-    trait_name = models.CharField(max_length=200)
+    trait_name = models.CharField(max_length=200, blank=True, null=True, )
+    trait_apiName = models.CharField(max_length=200 ,blank=True, null=True, )
     trait_img = models.ImageField(upload_to= 'gameimages/', blank=True,default='')
-    trait_champions_name = models.CharField(max_length=200)
-    trait_info = models.CharField(max_length=1000)
+    trait_effect = models.JSONField()
+    trait_info = models.CharField(max_length=1000, blank=True, null=True, )
     
+class Augment(models.Model):
+    augment_apiName= models.CharField(max_length=200, blank=True, null=True,)
+    augment_info = models.CharField(max_length=200, blank=True, null=True,)
+    augment_img = models.ImageField(upload_to='gameimages/', blank=True, default='')
+    augment_id = models.IntegerField()
+    augment_name = models.CharField(max_length=200, blank=True, null=True, )
+    augment_effect = models.JSONField()
 #챔피언 기본 정보
 class Champion(models.Model):
-    champion_name = models.CharField(max_length=200)
+    champion_apiName = models.CharField(max_length=300, blank=True, null=True, )
+    champion_variables = models.JSONField()
+    champion_name = models.CharField(max_length=200, blank=True, null=True, )
     champion_img = models.ImageField(upload_to='gameimages/', blank=True, default='')
-    champion_info = models.CharField(max_length=1000)
+    champion_info = models.CharField(max_length=1000, blank=True, null=True, )
     traits = models.ManyToManyField(
         Trait,
         related_name='trait'
     )
+    champion_stats = models.JSONField()
     champion_cost = models.IntegerField()
     
 #조합 아이템 정보
 class BaseItem(models.Model):
-    item_name = models.CharField(max_length=200)
+    item_id = models.IntegerField()
+    item_name = models.CharField(max_length=200,blank=True, null=True, )
     item_img = models.ImageField(upload_to='gameimages/', blank=True, default='')
-    item_info = models.CharField(max_length=1000)
+    item_info = models.CharField(max_length=1000, blank=True, null=True, default='')
+    item_apiName = models.CharField(max_length=200,blank=True, null=True, )
+    item_effect = models.JSONField()
     
 #완성 아이템 및 오른 아이템 정보
 class UpperItem(models.Model):
-    item_name = models.CharField(max_length=200)
+    item_id = models.IntegerField()
+    item_name = models.CharField(max_length=200,blank=True, null=True, )
     item_img = models.ImageField(upload_to='gameimages/', blank=True, default='')
-    item_info = models.CharField(max_length=1000)
+    item_info = models.CharField(max_length=1000, blank=True, null=True, default='')
+    item_apiName = models.CharField(max_length=200,blank=True, null=True, )
+    item_effect = models.JSONField()
     base_items = models.ManyToManyField(
         BaseItem,
         related_name = 'base_item'
