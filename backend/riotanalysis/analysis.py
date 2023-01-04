79ca6ed8dbe54fb77940e81222f6bb0d4030b7e4
RYU <ganggupamail@gmail.com>
2023-01-04 21:32:32 +0900

    augments 종류 모으기

--- backend/riotanalysis/analysis.py
+++ backend/riotanalysis/analysis.py
@@ -12,6 +12,7 @@ import django
 django.setup()
 from riotanalysisapp.models import MatchData
 
+"""
 df = MatchData.objects.values('last_level')
 last_levels = []
 for level in df:
@@ -27,4 +28,26 @@ for place in df:
     else:
         istop4 = True
     top4.append(istop4)
+"""
+# augments 전체 데이터
+df = MatchData.objects.values('augments')
+length = len(df)
+augments_list = []
+
+for i in range(length):
+    data = df[i]['augments']
+    data = data.split('"')
+    data = [item for item in data if len(item) > 2]
+
+    for item in data:
+        augments_list.append(item)
+
+augments_list = list(set(augments_list))
+# print(len(augments_list)) : current 272
+
+target = augments_list[0]
+print(target)
+
+
+
 
