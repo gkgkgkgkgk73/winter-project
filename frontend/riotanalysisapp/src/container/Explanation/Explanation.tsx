adf572f8fc4ee9757b799c425beb7b0639d213a4
gkgkgkgkgk73 <gkgkgkgkgk73@gmail.com>
2023-01-09 14:44:33 +0900

    프론트 api 연동 진행 중

--- frontend/riotanalysisapp/src/container/Explanation/Explanation.tsx
+++ frontend/riotanalysisapp/src/container/Explanation/Explanation.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import './Explanation.css';
 import { AppDispatch } from '../../store';
@@ -6,17 +6,38 @@ import Header from '../../component/Header/Header';
 import ItemInfo from '../../component/ItemInfo/ItemInfo';
 import DecInfo from '../../component/DecInfo/DecInfo';
 import AugmentInfo from '../../component/AugmentInfo/AugmentInfo';
+import { fetchAugments, fetchChampions, fetchItems, fetchTraits, selectAPI } from '../../store/slices/riotAPI';
+import { useSelector } from 'react-redux';
 
 function Explanation(){
 
     const dispatch = useDispatch<AppDispatch>();
+    const augment = useSelector(selectAPI).augment;
+    const upperitem = useSelector(selectAPI).item.upperitem;
+    const baseitem = useSelector(selectAPI).item.baseitem;
+    const champion = useSelector(selectAPI).champion;
+    const trait = useSelector(selectAPI).trait;
+
     const dd = [""]
+    useEffect(()=>{
+        dispatch(fetchAugments())
+        dispatch(fetchChampions())
+        dispatch(fetchItems())
+        dispatch(fetchTraits())
+    })
     return(
         <div className='page'>
             <Header />
             <ItemInfo itemName="itemName" itemImg="itemImg" itemDetail="itemDetail" itemCombination={dd} />
             <DecInfo decName={dd} decImg={dd} decDetailUnit={dd} decDetailUnitImg={dd} decInfo="decInfo"/>
             <AugmentInfo augmentName="augmentName" augmentImg="augmentImg" augmentInfo="augmentInfo"/>
+            <div>
+                <p>augment:{augment[0].name}</p>
+                <p>upperitem:{upperitem[0].name}</p>
+                <p>baseitem:{baseitem[0].name}</p>
+                <p>champion:{champion[0].name}</p>
+                <p>trait:{trait[0].name}</p>
+            </div>
         </div>
     );
 }
--- frontend/riotanalysisapp/src/store/slices/riotAPI.ts
+++ frontend/riotanalysisapp/src/store/slices/riotAPI.ts
@@ -2,48 +2,177 @@ import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { RootState } from "..";
 
-export interface RiotItemType{
+export interface RiotUpperItemType{
     id:number;
+    name:string;
+    img:string;
+    info:string;
+    apiName:string;
+    effect:JSON;
+    baseItems:number[];
+}
+
+export interface RiotBaseItemType{
+    id:number;
+    name:string;
+    img:string;
+    info:string;
+    apiName:string;
+    effect:JSON;
 }
 
 export interface ItemState {
-    item:RiotItemType[];
+    upperItems:RiotUpperItemType[];
+    baseItems:RiotBaseItemType[];
 }
 
-const initialItemState: ItemState ={
-    item:[],
+export interface RiotTraitType{
+    name:string;
+    apiName:string;
+    img:string;
+    effect:JSON;
+    info:string;
 }
 
-// const initailState: = {
-// }
+export interface TraitState {
+    traits:RiotTraitType[];
+}
+
+export interface RiotChampionType {
+    apiName:string;
+    variables:JSON;
+    name:string;
+    img:string;
+    info:string;
+    traits:number[];
+    championStats:JSON;
+    championCost:number;
+}
+
+export interface ChampionState {
+    champions:RiotChampionType[];
+}
+
+export interface RiotAugmentType{
+    apiName:string;
+    info:string;
+    img:string;
+    id:number;
+    name:string;
+    effect:JSON;
+}
+
+export interface AugmentState {
+    augments:RiotAugmentType[];
+}
+
+export interface BasicGameInfo {
+    item:ItemState;
+    champion:ChampionState;
+    augment:AugmentState;
+    trait:TraitState;
+}
+
+const initialState:BasicGameInfo= {
+    item:{
+        upperItems:[],
+        baseItems:[]
+    },
+    champion:{
+        champions:[]
+    },
+    augment:{
+        augments:[]
+    },
+    trait:{
+        traits:[]
+    }
+}
 
 export const fetchItems = createAsyncThunk(
     "item/fetchItems",
     async()=>{
-        const response = await axios.get<RiotItemType[]>("/api/items/");
+        const response = await axios.get<{upper_items:RiotUpperItemType[], base_items:RiotBaseItemType[]}>("/api/items/");
         return response.data;
     }
 )
 
-// export const riotAPI = createSlice({
-//     name:'riotAPIs',
-//     initialState,
-//     reducers:{
+export const fetchAugments = createAsyncThunk(
+    "augment/fetchAugments",
+    async()=>{
+        const response = await axios.get<RiotAugmentType[]>("/api/augments/");
+        return response.data;
+    }
+)
+
+export const fetchTraits = createAsyncThunk(
+    "trait/fetchTraits",
+    async()=>{
+        const response = await axios.get<RiotTraitType[]>("/api/traits/");
+        return response.data;
+    }
+)
+
+export const fetchTrait = createAsyncThunk(
+    "trait/fetchTrait",
+    async(id:number) => {
+        const response = await axios.post<RiotTraitType>("/api/trait/",id)
+        return response.data;
+    }
+)
+
+export const fetchChampions = createAsyncThunk(
+    "champion/fetchChampions",
+    async()=>{
+        const response = await axios.get<RiotChampionType[]>("/api/champions/");
+        return response.data;
+    }
+)
+
+export const riotAPI = createSlice({
+    name:'riotAPIs',
+    initialState,
+    reducers:{
         
-//     },
-//     extraReducers: (builder) => {
-//         builder.addCase(fetchItems.fulfilled, (state, action)=>{
-//             state.item = action.payload;
-//         });
-//         builder.addCase(fetchItems.rejected, (state, action)=>{
-//             state.item = null
-//         });
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchItems.fulfilled, (state, action)=>{
+            state.item.upperItems = action.payload.upper_items;
+            state.item.baseItems = action.payload.base_items;
+        });
+        builder.addCase(fetchItems.rejected, (state, action)=>{
+            state.item = {
+                upperItems:[],
+                baseItems:[]
+            }
+        });
 
-//     }
+        builder.addCase(fetchAugments.fulfilled, (state, action)=>{
+            state.augment.augments = action.payload;
+        });
+        builder.addCase(fetchItems.rejected, (state, action)=>{
+            state.augment.augments = [];
+        });
 
-// })
+        builder.addCase(fetchTraits.fulfilled, (state, action)=>{
+            state.trait.traits = action.payload;
+        });
+        builder.addCase(fetchItems.rejected, (state, action)=>{
+            state.trait.traits = [];
+        });
 
-// export const riotAPIActions = riotAPI.actions;
-// export const selectAPI = (state:RootState) => state.riotAPI;
+        builder.addCase(fetchChampions.fulfilled, (state, action)=>{
+            state.champion.champions = action.payload;
+        });
+        builder.addCase(fetchChampions.rejected, (state, action)=>{
+            state.champion.champions= [];
+        });
 
-// export default riotAPI.reducer;
+    }
+
+})
+
+export const riotAPIActions = riotAPI.actions;
+export const selectAPI = (state:RootState) => state.riotAPI;
+
+export default riotAPI.reducer;
