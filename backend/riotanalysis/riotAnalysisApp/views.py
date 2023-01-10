d8b57dbd52fde3ab7e5297c123845f7440f81be0
RYU <ganggupamail@gmail.com>
2023-01-10 15:51:30 +0900

    before the cafe

--- backend/riotanalysis/riotAnalysisApp/views.py
+++ backend/riotanalysis/riotAnalysisApp/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Trait, UpperItem, BaseItem, Champion, Augment
 from django.http import HttpResponse
 
+import pandas as pd
+from riotanalysisapp.models import MatchData, Augment
+from django.db.models import Q
+
 specific_item = ['영혼의 형상','죽음의 저항','영원한 겨울', '무한한 삼위일체', '마나자네', '흑요석 양날 도끼', '란두인의 성소', '로켓 주먹', '황금 징수의 총', '존야의 역설', '우르프 천사의 지팡이', '축복받은 피바라기', '푸른 축복', '장미가시 조끼', '선의의 성배', '빛나는 죽음의 검', '용의 의지', '철갑의 서막', '수호상 돌갑옷', '악마 학살자', '새벽의 서광', '구인수의 심판', '공정의 주먹', '마법공학 생명검', '천공의 대검', '집단 충격기', '눈부신 건틀릿', '영겁의 속삭임', '타곤 정상의 펜던트', '엔젤로노미콘', '반짝이는 수은', '라바돈의 초월한 죽음모자', '고속 광자포', '면죄', '루난의 폭풍', '경외의 장막', '히라나의 창', '스태틱의 호의', '태양빛 망토', '장난꾸러기의 장갑', '거인의 맹세', '의지파괴자', '워모그의 자부심', '지크의 조화', '겨울바람', '즈롯 소환문', '메카 선택기', '에이스 상징', '방패대 상징', '톱날 피바라기', '싸움꾼 상징', '주문투척자 상징', '민간인 상징', '특등사수 상징', '엄호대 상징', '메카: 프라임 상징', '기계유망주 상징', '과부하_오류 // 거인 학살자', '해커 상징', '비결정적 정의의 손길', '자화 이온 충격기', '익살꾼 상징', '용수철이 든 고속 연사포', '정찰단 상징', '날뛰는 쇼진의 창', '더 고요한 침묵의 장막', '별 수호자 상징', '과열된 태양불꽃 망토', '지하세께 상징', '유도집전형 워모그의 갑옷', 'B.F.대검', '곡궁', '쇠사슬 조끼', '음전자 망토', '쓸데없이 큰 지팡이', '여신의 눈물', '거인의 허리띠', '연습용 장갑', '뒤집개']
 
 # Create your views here.
@@ -54,5 +58,70 @@ def get_trait_detail(request, trait_id):
             return HttpResponse(traits, status = 200)
         except:
             return HttpResponse(status = 404)
+    else:
+        return HttpResponse(status = 403)
+
+def get_augment_stat(request):
+    if request.method == "GET":
+        # augments 전체 데이터
+        augments_str = ['augments1','augments2','augments3']
+
+        for j in range(len(augments_str)):
+            df = MatchData.objects.values(augments_str[j])
+            length = len(df)
+            augments_list = []
+
+            for i in range(length):
+                data = df[i]['augments'+ str(j+1)]
+                augments_list.append(data)
+
+        augments_list = list(set(augments_list))
+        #print((augments_list)) #: current 272
+        zero_inplated_augments_list = augments_list[1:]
+
+
+        data = []
+        for i in range(len(zero_inplated_augments_list)):
+            target = zero_inplated_augments_list[i]
+            
+            queryset = MatchData.objects.all().values('id', 'placement', 'augments1')
+            total_games_num = len(queryset)
+            result = queryset.filter( (Q(augments1 = target)) & Q(game_type = 'standard'))
+            pick_count = len(result)
+            
+            pick_rate = 0
+            if total_games_num > 0:
+                pick_rate = pick_count / total_games_num
+
+            win_count = 0
+            for item in result:
+                if item['placement'] < 5:
+                    win_count = win_count + 1
+
+            win_rate = 0
+            if len(result) > 0:
+                win_rate = win_count / len(result)
+
+            dat = [win_rate, pick_rate, win_count, len(result), target]
+            data.append(dat)
+            #print("Win rate is " + str(win_rate) + "/ " + str(win_count) + "/ " + str(len(result)))    
+
+            #if i > 9: break
+
+        df = pd.DataFrame(data, columns = ['win rate', 'pick rate', 'win count', 'total games', 'first_augment_name'])
+        df_upper30 = df.loc[df['total games'] > 29] # 30판 이상 기록만 걸러냄
+        #print(df_upper10.sort_values('total games', ascending=False))
+        #df.to_csv("C:/Users/gangg/win_rate.csv")
+
+        df_upper30_sorted_by_total_games = df_upper30.sort_values('total games', ascending=False)
+
+        kor_name_list = []
+        for name in df_upper30_sorted_by_total_games['first_augment_name'].to_list():
+            kor_name = Augment.objects.get(augment_apiName = name).augment_name
+            kor_name_list.append(kor_name)
+
+        df_upper30_sorted_by_total_games['kor_augment_name'] = kor_name_list
+
+        return HttpResponse(df_upper30_sorted_by_total_games, status = 200)
     else:
         return HttpResponse(status = 403)
--- frontend/riotanalysisapp/src/App.tsx
+++ frontend/riotanalysisapp/src/App.tsx
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Explanation from './container/Explanation/Explanation';
+import AugmentStat from './container/AugmentStat/AugmentStat';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Explanation/>}/>
+        <Route path='/augmentstat' element={<AugmentStat/>}/>
       </Routes>
     </BrowserRouter>
   );
